76e47bb6afb2e6d26269b9a7b6bcb608f70647cd
Catherine <c.abdmeziem@laposte.net>
2021-02-09 09:51:49 +0100

    mêmes comm que le précédent commit

--- index.php
+++ index.php
@@ -5,43 +5,65 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <!-- CDN Font Awesome -->
+  <script src="https://kit.fontawesome.com/07d23712a0.js" crossorigin="anonymous"></script>
+
   <!-- CDN Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
   <!-- Own styles -->
-  <link rel="stylesheet" href="assets/css/style.css">
+  <?php 
+    if (isset($_COOKIE['theme'])){ ?>
+      <link rel="stylesheet" href="assets/css/style<?= $_COOKIE['theme'] ?>.css">
+    <?php } else { ?>
+    <!-- Default style -->
+      <link rel="stylesheet" href="assets/css/style.css?v=1">
+    <?php } ?>
+  
   <title>Flux RSS</title>
 </head>
 
 <body>
   <?php
+    require "controllers/index_controller.php";
+
     $nbTopics = 5;
-    $tabTopics = [];
   ?>
 
   <!-- Navigation part (title + parameters button) -->
   <header>
     <h1 class="titleSite">Lecteur de flux RSS</h1>
     <nav>
-      </nav>
-    </header>
-    
-    <div class="container-fluid">
-      <div class="row text-center">
-        <h2 class="titlePage">Les articles du flux RSS trucmuche</h2>
-      </div>
+      <a href="views/param.php">
+        <i class="fas fa-cog"></i>
+      </a>
+    </nav>
+  </header>
+
+  <div class="container-fluid">
+    <div class="row text-center">
+      <h2 class="titlePage">Les articles du flux RSS trucmuche</h2>
+    </div>
     <?php
-      for ($i=0; $i<=$nbTopics; $i++){ ?>
+      for ($i=0; $i<$nbTopics; $i++){ ?>
         <div class="row">
-          <div class="col indexColImg">
-            <img src="assets/img/rss640.png" alt="img de l'article">
+          <div class="col-1">
+            <i class="fas fa-rss-square"></i>
           </div>
-          <div class="col indexCol"></div>
+          <div class="col indexTopicTitle">Voici le titre de l'article <?= $i ?></div>
+          <div class="col-2 indexTopicDate">08/02/2021 20h24</div>
+          <div class="col-1">
+            <button type="button" class="indexTopicInfos" data-toggle="modal" data-target="#exampleModal">+ infos</button>
+          </div>
         </div>
       <?php } ?>
   </div>
 
   <!-- Bootstrap scripts -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+  <!-- own scripts -->
+  <script src="assets/js/scripts.js"></script>
+
 </body>
 </html>
---
+++ views/param.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- CDN Font Awesome -->
+  <script src="https://kit.fontawesome.com/07d23712a0.js" crossorigin="anonymous"></script>
+
+  <!-- CDN Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+  <!-- Own styles -->
+  <link rel="stylesheet" href="assets/css/style.css?v=1">
+  <title>Flux RSS</title>
+</head>
+
+<body>
+
+  <header>
+    <h1 class="titleSite">Lecteur de flux RSS</h1>
+    <nav>
+      <ul class="navLst">
+      <a href="views/param.php" class="navLink">
+        <i class="fas fa-cog"></i>
+      </a>
+      <a href="../index.php" class="navLink">
+        Retour page accueil
+      </a>
+      </ul>
+
+    </nav>
+  </header>
+
+
+  
+  <!-- Bootstrap scripts -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+  <!-- own scripts -->
+  <script src="assets/js/scripts.js"></script>
+
+</body>
+</html>
